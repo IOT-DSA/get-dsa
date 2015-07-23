b83b3e6c59bf8403cee99bfc8f48c2016b6b41db
Kenneth Endfinger <kaendfinger@gmail.com>
2015-07-23 13:58:32 -0400

    Bug Fix

--- lib/packager.dart
+++ lib/packager.dart
@@ -13,7 +13,7 @@ const String BATCH_DART_WRAPPER = r"""
 @echo off
 set me=%~f0
 set me=%me:~0,-4%
-%~0\..\..\..\dart-sdk\bin\dart.exe %me%.dart %*
+%~0\..\..\..\dart-sdk\bin\dart.exe "%me%.dart" %*
 """;
 
 class DSLinkPackage {
